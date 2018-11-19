84486ae2911b428290b8c408c15308ef17babb5f
Samuli Virtanen <samuliedvin@gmail.com>
2018-11-19 19:24:01 +0200

    Add solution to exercises 3.1.-3.6.

--- index.js
+++ index.js
@@ -65,7 +65,6 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    console.log(request.body)
     if(!body.name) {
         return response.status(400).json({error: 'name missing'})
     }
