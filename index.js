df75765a944cdc5e0a878c69e804c9ac89b597d5
Samuli Virtanen <samuliedvin@gmail.com>
2018-11-19 19:16:19 +0200

    Add solution to exercises 3.1.-3.6.

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+    {
+        name: 'Arto Hellas',
+        number: '040-123456',
+        id: 1
+    },
+    {
+        name: 'Martti Tienari',
+        number: '040-123456',
+        id: 2
+    },
+    {
+        name: 'Arto Järvinen',
+        number: '040-123456',
+        id: 3
+    },
+    {
+        name: 'Lea Kutvonen',
+        number: '040-123456',
+        id: 4
+    }
+]
+  
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+    let personCount = persons.length  
+    let date = new Date()
+    let result = `
+    <p>puhelinluettelossa ${personCount} henkilön tiedot<p>
+    <p>${date}</p>
+    `
+    res.send(result)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if ( person ) {
+    response.json(person)
+    } else {
+    response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(request.body)
+    if(!body.name) {
+        return response.status(400).json({error: 'name missing'})
+    }
+    if(!body.number) {
+        return response.status(400).json({error: 'number missing'})
+    }
+    if(persons.find(person => person.name === body.name)) {
+        return response.status(400).json({error: 'name must be unique'})
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random()*1000000)
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
